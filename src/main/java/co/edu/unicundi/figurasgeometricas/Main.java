3d594d51b9986fa5ab92746060ffd10d36a14b73
ltramos14 <48187187+ltramos14@users.noreply.github.com>
2021-03-17 12:41:57 -0500

    Primer commit de la clase Main

---
+++ src/main/java/co/edu/unicundi/figurasgeometricas/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.edu.unicundi.figurasgeometricas;
+
+/**
+ * 
+ * @author Tatiana Ramos Villanueva
+ * @author Nicolás Nieto Cárdenas
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
