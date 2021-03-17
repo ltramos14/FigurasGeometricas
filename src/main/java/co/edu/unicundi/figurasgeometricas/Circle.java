6eccb746cc6ca18e978b4410e238972530630cfa
ltramos14 <tatianarv14@gmail.com>
2021-03-17 17:27:27 -0500

    Metodos para calcular area y perimetro del Circulo

--- src/main/java/co/edu/unicundi/figurasgeometricas/Circle.java
+++ src/main/java/co/edu/unicundi/figurasgeometricas/Circle.java
@@ -9,6 +9,30 @@ package co.edu.unicundi.figurasgeometricas;
  *
  * @author nicon
  */
-public class Circle {
+public class Circle extends Figure{
+    
+    int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void calculateArea() {
+        area = Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public void calculatePerimeter() {
+       perimeter = 2 * Math.PI * radius;      
+    }
+
+    @Override
+    public void printAreaAndPerimeter() {
+        calculateArea();
+        calculatePerimeter();
+        System.out.println("EL ÁREA DEL CÍRCULO ES: " + area);
+        System.out.println("EL PERÍMETRO DEL CÍRCULO ES: " + perimeter);
+    }
     
 }
